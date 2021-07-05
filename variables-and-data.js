48db9351b1d90b2d5c0b81f396d024d6af420054
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-07-05 07:11:39 -0600

    Added Javascript

---
+++ variables-and-data.js
@@ -0,0 +1,33 @@
+/*
+* Variables, Data Types and Typing
+ */
+let wordData = "Word Data";
+console.log(wordData);
+let subtotal = 100;
+let tax = subtotal *0.07;
+let totalTax = tax + subtotal;
+console.log(totalTax);
+
+
+
+let obj = {
+    key1: "value 1",
+    key2: 3,
+    obj2: {
+        obj2key1: "value pf inner object"
+    }
+};
+console.log(obj.obj2.obj2key1);
+
+let var1 = "2";
+let var2 = "5";
+let result = var1 + var2
+console.log(result);
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let multiplication = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+let string = "string1" + "string 2"
+console.log(string);
